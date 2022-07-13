7495639086f6bf2cae7093d1fec42d6643a5eb74
d0422 <rlfehd2013@naver.com>
2022-07-14 02:49:21 +0900

    :zap: 배열
    배열

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
 package Java;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helloworld");
+        int arr[][] = { { 1, 2, 3 }, { 4, 5 }, { 6 } };
+        for (int num[] : arr) {
+            for (int x : num) {
+                System.out.println(x);
+            }
+        }
     }
 }
