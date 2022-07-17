9cc50a2b3af2a42c54af88f0cbf58ec1c5e8bd5a
d0422 <rlfehd2013@naver.com>
2022-07-17 11:35:31 +0900

    :sparkles: 다형성

--- project file/src/Animal.java
+++ project file/src/Animal.java
@@ -1,10 +1,37 @@
 public class Animal {
     String name;
 
-    void setName(String name) {
+    Animal(String name) {
         this.name = name;
+    }
+
+    void feed() {
+        System.out.println("bobbobbob!!");
+    }
+
+    void print() {
+        System.out.println("name :" + name);
     }
 }
 
 class Dog extends Animal {
+    int age;
+
+    Dog(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    void feed() {
+        System.out.println("walwalwal");
+    }
+
+    void animalFeed() {
+        super.feed();
+    }
+
+    void print() {
+        System.out.println("name :" + name);
+        System.out.println("age:" + age);
+    }
 }
--- project file/src/App.java
+++ project file/src/App.java
@@ -1,12 +1,27 @@
 public class App {
+    public void typeOfAnimal(Animal e) {
+        if (e instanceof Dog) {
+            System.out.println("Animal!!");
+        } else {
+            System.out.println("Dog!!");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        Animal a = new Animal();
-        a.setName("Cat");
-        Dog v = new Dog();
-        v.setName("Dog");
+
+        Animal a = new Animal("cat");
+        Dog v = new Dog("dog", 3);
+        a.print();
+        v.print();
         Bark b = new Bark();
+        App app = new App();
+        app.typeOfAnimal(a);
+        app.typeOfAnimal(v);
         b.b(v);
         b.b(a);
+        v.feed();
+        v.animalFeed();
+        a.feed();
         System.out.println(Sum.sumNum(1, 2, 3, 4, 5));
     }
 }
